ca92a443a46b236f385896fb72c1a97be32602ec
hsien <hoanplpyy@hotmail.com>
2022-09-01 22:53:17 +0800

    222: 將選取的項目，放入input表格中

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { Ingredient } from '../../shared/ingredient.model';
@@ -11,10 +11,13 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm:NgForm;
 
   subscription: Subscription;
   editMode = false;
   editItemIndex: number;
+
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -23,6 +26,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.editMode = true;
         this.editItemIndex = index;
+        this.editedItem=this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name:this.editedItem.name,
+          amount: this.editedItem.amount
+        })
       }
     )
   }
